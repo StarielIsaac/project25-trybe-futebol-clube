72515d6d12d46b0cc0bd1be23d657b04526940d4
Stariel Isaac <starielisaac@gmail.com>
2023-05-20 12:57:07 -0300

    feat: created method static 'validateTeamsAndExistence' in class MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,9 +1,16 @@
 import MatchModel from '../model/MatchModel';
 import updateInfo from '../types/updateInfo';
 import typeNewMatch from '../types/typeNewMatch';
+import ErrorLaunch from '../utils/errorLaunch';
 
 export default class MatchService {
   constructor(private matchModel = new MatchModel()) {}
+
+  static validateTeamsAndExistence(homeTeamId: number, awayTeamId: number) {
+    if (homeTeamId === awayTeamId) {
+      throw new ErrorLaunch('It is not possible to create a match with two equal teams', 422);
+    }
+  }
 
   async findAllMatches(inProgress: string | undefined) {
     if (inProgress !== undefined) {
@@ -27,9 +34,12 @@ export default class MatchService {
   }
 
   async createNewMatch({ homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals }: typeNewMatch) {
+    const isValid = MatchService.validateTeamsAndExistence(homeTeamId, awayTeamId);
+
     const match = await this.matchModel.createNewMatch(
       { homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals } as typeNewMatch,
     );
+
     return match;
   }
 }
